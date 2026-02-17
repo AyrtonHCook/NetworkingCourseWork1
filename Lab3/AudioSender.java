9deda5bc1f164545055eb0438d682cb4adc2d027
Bosco-13 <uft24kuu@uea.ac.uk>
2026-02-17 13:36:32 +0000

    Update AudioSender.java

--- Lab3/AudioSender.java
+++ Lab3/AudioSender.java
@@ -1,77 +1,69 @@
-/*
- * TextReceiver.java
- */
+import CMPC3M06.AudioRecorder;
 
-/**
- *
- * @author  abj
- */
+import javax.sound.sampled.LineUnavailableException;
+import java.io.IOException;
 import java.net.*;
-import java.io.*;
-import CMPC3M06.AudioPlayer;
 
-public class AudioSender implements Runnable{
-    
-    static DatagramSocket receiving_socket;
-    
+public class AudioSenderThread implements Runnable{
+
+    public DatagramSocket sending_socket;
+
     public void start(){
         Thread thread = new Thread(this);
-	thread.start();
+        thread.start();
     }
-    
-    public void run (){
-     
-        //***************************************************
-        //Port to open socket on
+
+    @Override
+    public void run() {
+        //initialise variables
         int PORT = 55555;
-        //***************************************************
-        
-        //***************************************************
-        //Open a socket to receive from on port PORT
-        
-        //DatagramSocket receiving_socket;
-        try{
-		receiving_socket = new DatagramSocket(PORT);
-	} catch (SocketException e){
-                System.out.println("ERROR: TextReceiver: Could not open UDP socket to receive from.");
-		e.printStackTrace();
-                System.exit(0);
-	}
-        //***************************************************
-        
-        //***************************************************
-        //Main loop.
-        
-        // create audio player
-        AudioPlayer player = null;
-        try{
-            player = new AudioPlayer();
-        } catch (LineUnavailableException e) {
-            System.out.println("Error: cannot open audio player");
+        InetAddress clientIP = null;
+        AudioRecorder recorder = null;
+        byte[] block = null;
+        try {
+            // get client IP address
+            clientIP = InetAddress.getByName("0000.0000.0000.0000");
+        } catch (UnknownHostException e) {
+            System.out.println("Errorr: Counld not find client IP.");
+            System.exit(0);
         }
 
-        boolean running = true;
-        
-        while (running){
-            byte[] buffer = new byte[512];
-            DatagramPacket packet = new DatagramPacket(buffer, 0, 512);
+        try{
+            // create socket
+            sending_socket = new DatagramSocket();
+        } catch (SocketException e) {
+            System.out.println("Error: Unable to open socket.");
+            System.exit(0);
+        }
+
+        try {
+            // create audio recorder
+            recorder = new AudioRecorder();
+        } catch (LineUnavailableException e) {
+            System.out.println("Error: Unable to open AudioRecorder.");
+            System.exit(0);
+        }
+
+        System.out.println("Session start.");
+        while(true){
+            try{
+                // get payload of the packet
+                block = recorder.getBlock();
+            } catch (IOException e) {
+                System.out.println("Error: IO exception occurred.");
+                System.exit(0);
+            }
 
             try{
-
-                receiving_socket.receive(packet);
-                player.playback(packet.getData());
-                
-                //The user can type EXIT to quit
-                if (str.substring(0,4).equals("EXIT")){
-                     running=false;
-                }
-            } catch (IOException e){
-                System.out.println("ERROR: TextReceiver: Some random IO error occured!");
-                e.printStackTrace();
+                // create and send packet
+                DatagramPacket packet = new DatagramPacket(block, block.length, clientIP, PORT);
+                sending_socket.send(packet);
+            } catch (IOException e) {
+                System.out.println("Error: IO exception occurred at packet sending.");
+                System.exit(0);
             }
         }
-        //Close the socket
-        receiving_socket.close();
-        //***************************************************
     }
+
 }
+..
