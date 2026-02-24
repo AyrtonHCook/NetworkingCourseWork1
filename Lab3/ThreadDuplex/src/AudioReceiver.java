c25f492ef5e07701537333ef705508dd55e15f2b
Bosco-13 <uft24kuu@uea.ac.uk>
2026-02-24 11:45:33 +0000

    Implement sender and receiver authentication; Update AudioReceiver.java

--- Lab3/ThreadDuplex/src/AudioReceiver.java
+++ Lab3/ThreadDuplex/src/AudioReceiver.java
@@ -1,3 +1,4 @@
+
 import CMPC3M06.AudioPlayer;
 /*
  * TextReceiver.java
@@ -7,11 +8,12 @@ import javax.sound.sampled.LineUnavailableException;
 import java.io.IOException;
 import java.net.*;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class AudioReceiver implements Runnable{
-    
+
     static DatagramSocket receiving_socket;
-    
+
     public void start(){
         Thread thread = new Thread(this);
         thread.start();
@@ -21,24 +23,25 @@ public class AudioReceiver implements Runnable{
     public void run() {
         //initialise variables
         int PORT = 55555;
+        short authenticationKey = 10;
         //***************************************************
-        
+
         //***************************************************
         //Open a socket to receive from on port PORT
-        
+
         //DatagramSocket receiving_socket;
         try{
-		receiving_socket = new DatagramSocket(PORT);
-	} catch (SocketException e){
-                System.out.println("ERROR: TextReceiver: Could not open UDP socket to receive from.");
-		e.printStackTrace();
-                System.exit(0);
-	}
+            receiving_socket = new DatagramSocket(PORT);
+        } catch (SocketException e){
+            System.out.println("ERROR: TextReceiver: Could not open UDP socket to receive from.");
+            e.printStackTrace();
+            System.exit(0);
+        }
         //***************************************************
-        
+
         //***************************************************
         //Main loop.
-        
+
         AudioPlayer player = null;
         try{
             player = new AudioPlayer();
@@ -50,17 +53,29 @@ public class AudioReceiver implements Runnable{
         boolean running = true;
         int key = 15;
         while (running){
-         
+
             try{
 
-                
 
-                byte[] buffer = new byte[512];
-                DatagramPacket packet = new DatagramPacket(buffer, 0, 512);
+
+                byte[] buffer = new byte[514];
+                DatagramPacket packet = new DatagramPacket(buffer, 0, 514);
                 receiving_socket.receive(packet);
 
-                byte[] encrypted = packet.getData();
-                int len = packet.getLength();
+                byte[] receivedPacket = packet.getData();
+                // getting header and checking the authentication key
+                short header = (short) (((receivedPacket[0] & 0xFF) << 8) | (receivedPacket[1] & 0xFF));
+                if(header != authenticationKey){
+                    System.out.println("Error: authentication failed");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("Sender varified");
+                }
+                // removing the header from the packet
+                byte[] encrypted = Arrays.copyOfRange(receivedPacket, 2, receivedPacket.length);
+
+                int len = encrypted.length;
 
                 ByteBuffer unwrapDecrypt = ByteBuffer.allocate(len);
                 ByteBuffer cipherText = ByteBuffer.wrap(encrypted);
@@ -69,10 +84,10 @@ public class AudioReceiver implements Runnable{
                     int fourByte = cipherText.getInt();
                     fourByte = fourByte ^ key;
                     unwrapDecrypt.putInt(fourByte);
-                    }
+                }
 
                 byte[] decryptedBlock = unwrapDecrypt.array();
-                
+
                 player.playBlock(decryptedBlock);
 
             } catch (IOException e){
@@ -84,4 +99,3 @@ public class AudioReceiver implements Runnable{
     }
 
 }
-
